88ee1deb0c6125536eebd0af24735ab231e1fc9f
WoongHui Lee <comyoung21@gmail.com>
2019-12-10 14:05:56 +0900

    하울의 SNS 제3강 구글 입력 로그인 성공 reference.txt 파일 안에 중요사항 정리

--- HowlsLecture/app/src/main/java/com/example/howlslecture/LoginActivity.kt
+++ HowlsLecture/app/src/main/java/com/example/howlslecture/LoginActivity.kt
@@ -4,14 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var auth : FirebaseAuth
-
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_lOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +27,15 @@ class LoginActivity : AppCompatActivity() {
         emailloginButton.setOnClickListener {
             createloginandemail()
         }
+        google_sign_in_button.setOnClickListener {
+            googleLogin()
+        }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()   // 코드를 끝내겠다. 조립 완성
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
     }
     fun createloginandemail() {
         auth.createUserWithEmailAndPassword(
@@ -60,6 +76,24 @@ class LoginActivity : AppCompatActivity() {
                 finish()
             }
         }
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_lOGIN_CODE)
 
+    }
+    fun firebaseAuthWithGoogle(accout : GoogleSignInAccount){
+        var credential = GoogleAuthProvider.getCredential(accout.idToken,null)
+        auth.signInWithCredential(credential)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==GOOGLE_lOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                var accout = result.signInAccount
+                firebaseAuthWithGoogle(accout!!)
+            }
+        }
+    }
 
 }
