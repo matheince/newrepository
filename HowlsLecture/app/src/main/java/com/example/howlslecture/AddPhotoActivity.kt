d3eb822fb166580d2d0db6b473432271d2a1d3ca
WoongHui Lee <comyoung21@gmail.com>
2019-12-12 17:06:07 +0900

    하울의 SNS 제7강 Firebase database에 사진 및 정보올리기 reference.txt 파일 안에 중요사항 정리, \ref_image 에 정보가 되는 사진파일 올림

--- HowlsLecture/app/src/main/java/com/example/howlslecture/AddPhotoActivity.kt
+++ HowlsLecture/app/src/main/java/com/example/howlslecture/AddPhotoActivity.kt
@@ -6,22 +6,29 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.howlslecture.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_add_photo.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class AddPhotoActivity : AppCompatActivity() {
     val PICK_IMAGE_FROM_ALBUM = 0
     var storage : FirebaseStorage? = null
     var photoUri : Uri? = null
+    var auth : FirebaseAuth? = null //FirebaseAuth 선언
+    var firestore : FirebaseFirestore? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
-        storage = FirebaseStorage.getInstance()
-
+        storage = FirebaseStorage.getInstance()     // FirebaseStorage 초기화
+        auth = FirebaseAuth.getInstance()           // FirebaseAuth 초기화
+        firestore = FirebaseFirestore.getInstance() // FirebaseFirestore  초기화
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
         photoPickerIntent.type = "image/*"
         startActivityForResult(photoPickerIntent,PICK_IMAGE_FROM_ALBUM)
@@ -54,8 +61,33 @@ class AddPhotoActivity : AppCompatActivity() {
         val timeStamp = SimpleDateFormat("yyyymmdd_hhmmss").format(Date())
         val imageFileName = "JPEG_" + timeStamp + "_.png"
         val storageRef = storage?.reference?.child("images")?.child(imageFileName)
-        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener { taskSnapshot ->
+
+
+
             Toast.makeText(this,getString(R.string.upload_success),Toast.LENGTH_LONG).show()
+            // 업로드된 이미지 주소
+            var contentDTO = ContentDTO()
+            var uri = taskSnapshot.uploadSessionUri
+
+
+            // 이미지 주소
+            contentDTO.imageUrl = uri!!.toString()
+
+            // 유저의 UID
+            contentDTO.uid =auth?.currentUser?.uid
+
+            // 게시물 설명
+            contentDTO.explain = addphoto_explain.text.toString()
+
+            // 유저 아이디
+            contentDTO.userID = auth?.currentUser?.email
+
+            // 게시물 업로드 시간
+            contentDTO.timestamp = System.currentTimeMillis()
+            firestore?.collection("images")?.document()?.set(contentDTO)
+            setResult(Activity.RESULT_OK)
+            finish()
         }
 
     }
