0e0293e76168c84bf0c87d3796c3b34a4b85f4df
WoongHui Lee <comyoung21@gmail.com>
2019-12-12 20:54:48 +0900

    하울의 SNS 제7-1강 RecyclerView를 이용 홈버튼 구현 - 바보스러움.... reference.txt 파일 안에 중요사항 정리, \ref_image 에 정보가 되는 사진파일 올림

--- HowlsLecture/app/src/main/java/com/example/howlslecture/DetailViewFragment.kt
+++ HowlsLecture/app/src/main/java/com/example/howlslecture/DetailViewFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_detail.view.*
 
 class  DetailViewFragment : Fragment(){
     override fun onCreateView(
@@ -12,6 +16,27 @@ class  DetailViewFragment : Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return LayoutInflater.from(inflater.context).inflate(R.layout.fragment_detail,container,false)
+        var view = LayoutInflater.from(inflater.context).inflate(R.layout.fragment_detail,container,false)
+        view.detailview_recyclerview.adapter = DetailRecyclerViewAdapter()
+        view.detailview_recyclerview.layoutManager = LinearLayoutManager(activity)
+        return view
+
+    }
+    inner class DetailRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view =LayoutInflater.from(parent.context).inflate(R.layout.item_detail, parent, false)
+            return CustomDetailHolder(view)
+        }
+
+        private inner class CustomDetailHolder(view:View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return 3
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            (holder as CustomDetailHolder).itemView
+        }
+
     }
 }
